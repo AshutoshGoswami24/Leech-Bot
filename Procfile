1126abfef474673400c6bb9bddff5d6a609cbca2
Ashutosh Goswami <160889349+AshutoshGoswami24@users.noreply.github.com>
2024-03-15 09:30:51 +0530

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,1 @@
+worker: python3 Ashutosh_Goswami24.py
---
+++ config.py
@@ -0,0 +1,11 @@
+import re
+import os
+from os import getenv, environ
+
+      
+
+api_id = int(environ.get("API_ID", ""))
+api_hash = environ.get("API_HASH", "")
+ADMINS = int(environ.get("ADMINS", ""))
+bot_token = environ.get("BOT_TOKEN", "")
+PICS = (environ.get('PICS', '')).split()
