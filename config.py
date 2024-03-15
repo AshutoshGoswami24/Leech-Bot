3737c215866d855dff32b5169de3d1092926dbb7
Ashutosh Goswami <160889349+AshutoshGoswami24@users.noreply.github.com>
2024-03-15 12:01:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,5 +7,5 @@ from os import getenv, environ
 api_id = int(environ.get("API_ID", "29917436"))
 api_hash = environ.get("API_HASH", "4a926822b076a086a167fe8f2701d3e9")
 ADMINS = int(environ.get("ADMINS", "6141937812"))
-bot_token = environ.get("BOT_TOKEN", "6524542652:AAHRJU8xodgF4Tog2vxrAdi9diopVrLOqBc")
+bot_token = environ.get("BOT_TOKEN", "7150830210:AAHK0PWU_1yd6guoCOCLvFJEy4LM2SNOY0M")
 PICS = (environ.get('PICS', 'https://graph.org/file/7bf517f440a124dc68d80.jpg https://graph.org/file/c42f1ed5e9e9f134b9ddc.jpg https://graph.org/file/a24f0b32ead8fde3adc21.jpg https://graph.org/file/a89bd2adf24a1b1f77bab.jpg https://graph.org/file/80f5cbdf1a7c47e77b490.jpg')).split()
