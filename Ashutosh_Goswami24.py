f09806ae30ef63031fc36b230dc38afb8689b72c
Ashutosh Goswami <160889349+AshutoshGoswami24@users.noreply.github.com>
2024-03-15 11:47:10 +0530

    Update Ashutosh_Goswami24.py

--- Ashutosh_Goswami24.py
+++ Ashutosh_Goswami24.py
@@ -4,7 +4,6 @@ import subprocess
 from datetime import datetime
 from pyrogram import Client, filters
 from config import *
-
 # Initialize the bot
 app = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 
